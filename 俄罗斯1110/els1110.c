a67c97cade4f807aca5785cf9a302a5a637a326e
卢孙仲 <1368062294@qq.com>
2017-10-28 19:05:45 +0800

    add a code

---
+++ 俄罗斯1110/els1110.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+//题号：1110，俄罗斯acm 
+int main()
+{
+	long int n,m,y,i,x,j;//对应题目中的n,m,y;
+	int k=0;
+	scanf("%d %d %d",&n,&m,&y);
+	for (x=0; x<m; x++){
+		i=x;
+		for (j=2; j<=n; j++){
+			i*=x;
+			i=i%m;
+		}
+		if (i==y){
+				k=1;
+				printf("%d ",x);
+		}
+	}
+	if(k==0)
+	printf("-1");
+	return 0;
+} 
