48f11c4474f44a365cc00210dbc57168e38bb45e
卢孙仲 <1368062294@qq.com>
2017-10-29 12:05:12 +0800

    change

--- els1068.c
+++ els1068.c
@@ -1,24 +1,26 @@
 #include<stdio.h>
-#include<math.h>
-int  main()
+int main()
 {
-		long int num,sum=0;
-		scanf("%d",&num);
-    	if(num==0)
-    	{
-	    	printf("%ld",1);
-	    	return 0;
-	    }
-	    if(num>=1)
-        {
-        	sum=num*(num+1)/2;
-        }
-		else{
-			num=(-num);
-			sum=num*(num+1)/2;
-			sum--;
-			sum=(-sum);
-		}
-		printf("%ld\n",sum);
+	long int num,sum;
+	scanf("%ld",&num);
+	//零 
+	if(num==0)
+	{
+		printf("1");
+		return 0;
+	}
+	//正整数 
+	if(num>=1)
+	{
+		sum=num*(num+1)/2;
+	}
+	//负整数 
+	else{
+		num=-num;
+		sum=num*(num+1)/2;
+		sum--;
+		sum=(-sum);
+	}
+	printf("%ld",sum);
 	return 0;
-} 
+}
